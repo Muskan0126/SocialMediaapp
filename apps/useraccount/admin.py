9938915607585921efa8cc06a93114dca215f8cf
Muskan0126 <muskan.agrawal@thoughtwin.com>
2026-07-16 17:14:42 +0530

    - Social Media App   - fixed duplicate posts button functionality

--- apps/useraccount/admin.py
+++ apps/useraccount/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from apps.useraccount.models import User
+
+from apps.useraccount.models import Subscription, User
+
 # Register your models here.
+admin.site.register(Subscription)
+
 
 @admin.register(User)
-
 class usercre(admin.ModelAdmin):
-    list_display = ('username', 'email', 'phone_no', 'gender','profile_photo')
-    ordering = ('-date_updated',)
+    list_display = ("username", "email", "phone_no", "gender", "profile_photo")
+    ordering = ("-date_updated",)
